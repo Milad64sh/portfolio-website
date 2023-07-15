f28b522a6a0e80c23a2a810ab52524fe06a5dd10
Milad Shalikarian <m.shalikarian@gmail.com>
2023-07-15 17:34:05 +0100

    second commit general structure

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import styles from '../styles/main.module.scss';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to My Portfolio</h1>
+      {/* Add your portfolio content here */}
+    </div>
+  );
+};
+
+export default Home;
